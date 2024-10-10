1818a574655e7ed40a609cd6e82f047c22fa5ac0
sunnyday-93 <56576834+sunnyday-93@users.noreply.github.com>
2024-10-10 21:15:44 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "google" {
+  project = "cicd-project-438005"
+  region  = "asia-northeast1"
+  credentials = file("./terraform-key.json")  # Path to service account JSON key
+}
+
+
+# Define the GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "terraform-autoscale-cluster"
+  location = "asia-northeast1"
+  initial_node_count = 1  # Still needed for the cluster itself, but the node pool is defined separately
+  
+  # Enable cluster autoscaling
+  remove_default_node_pool = true  # Remove the default node pool to define a custom one
+}
+
+# Define the node pool with autoscaling enabled
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = 1
+
+  autoscaling {
+    min_node_count = 1     # Minimum number of nodes
+    max_node_count = 3     # Maximum number of nodes
+  }
+
+  node_config {
+    machine_type = "e2-micro"     # Define node machine type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  # Define auto-scaling based on CPU utilization
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
+
+output "kubeconfig" {
+  value = google_container_cluster.primary.endpoint
+}
